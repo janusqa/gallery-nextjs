2b6b05df4fd2969bf075ca558cd7d8eb254ee280
Janus Developer <dev@retrievo.net>
2024-05-09 14:58:42 -0400

    update homepage with images and basic styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,10 +7,18 @@ const mockUrls = [
   "https://utfs.io/f/5a0b2b04-fd49-464e-aa70-08d2dc471db5-hdf3u3.jpg",
 ];
 
+const mockImages = mockUrls.map((url, index) => ({ id: index + 1, url }));
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      Hello (gallery in progress)
+    <main className="">
+      <div className="flex flex-wrap gap-4">
+        {mockImages.map((image) => (
+          <div key={image.id} className="w-48">
+            <img src={image.url} />
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
