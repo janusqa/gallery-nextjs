57af0596a39fd84c301810bf2b2f64f5f2174e33
Janus Developer <dev@retrievo.net>
2024-05-14 10:28:50 -0400

    fix drizzle

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,15 +4,14 @@ import { env } from "~/env";
 
 export default {
   schema: "./src/server/db/schema.ts",
-  driver: "aws-data-api",
   dialect: "postgresql",
   dbCredentials: {
-    // connectionString: env.POSTGRES_URL,
-    host: env.POSTGRES_HOST,
-    user: env.POSTGRES_USER,
-    password: env.POSTGRES_PASSWORD,
-    database: env.POSTGRES_DATABASE,
-    ssl: true,
+    url: env.POSTGRES_URL,
+    // host: env.POSTGRES_HOST,
+    // user: env.POSTGRES_USER,
+    // password: env.POSTGRES_PASSWORD,
+    // database: env.POSTGRES_DATABASE,
+    // ssl: true,
   },
   tablesFilter: ["t3gallery_*"],
 } satisfies Config;
