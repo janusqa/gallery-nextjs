fd705a7f2203eae70e5fbc63a4068ad01dc57a78
Janus Developer <dev@retrievo.net>
2024-05-15 10:39:59 -0400

    add Next Image component to replace regular image tags

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { SignedIn, SignedOut } from "@clerk/nextjs";
 import { getUserImages } from "~/server/queries";
+import Image from "next/image";
 
 export const dynamic = "force-dynamic";
 
@@ -7,10 +8,16 @@ async function Images() {
   const images = await getUserImages();
 
   return (
-    <div className="flex flex-wrap gap-4">
+    <div className="flex flex-wrap justify-center gap-4">
       {[...images].map((image) => (
-        <div key={image.id} className="flex w-48 flex-col">
-          <img src={image.url} />
+        <div key={image.id} className="flex h-48 w-48 flex-col">
+          <Image
+            src={image.url}
+            style={{ objectFit: "contain" }}
+            width={192}
+            height={192}
+            alt={image.name}
+          />
           <div>{image.name}</div>
         </div>
       ))}
